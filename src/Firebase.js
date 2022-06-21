bd2015e3a6b73b001a9470c03ee432b44d05f897
tarang sharma <74776793+trngapp@users.noreply.github.com>
2022-06-21 17:36:30 +0530

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -11,13 +11,13 @@
   import firebase from "firebase";
 
   const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyCUwhTPw3nGuYLAc5LDGjd4H058YkQqhNU",
-      authDomain: "todo-app-cp-1104b.firebaseapp.com",
-      projectId: "todo-app-cp-1104b",
-      storageBucket: "todo-app-cp-1104b.appspot.com",
-      messagingSenderId: "743080387969",
-      appId: "1:743080387969:web:f3eaff5cb9237229dcb480",
-      measurementId: "G-GHZ2D6Z0XJ"
+    apiKey: "AIzaSyBSix4XDEMiMB5s7frMjxCt7GinefNezWQ",
+  authDomain: "todo-8409a.firebaseapp.com",
+  projectId: "todo-8409a",
+  storageBucket: "todo-8409a.appspot.com",
+  messagingSenderId: "690043629379",
+  appId: "1:690043629379:web:a2567ef5041a6a8ccb09f6",
+  measurementId: "G-5FJYSGCHQ5"
 
   });
   const db =firebaseApp.firestore();
